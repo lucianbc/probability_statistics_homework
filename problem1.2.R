787a15086cfb2724f095e094b7be585b22287973
Lucian Boaca <lucianboaca97@gmail.com>
2018-02-05 02:07:04 +0200

    Sample for 1.2

---
+++ problem1.2.R
@@ -0,0 +1,29 @@
+plotBin <- function() {
+  # plots probability mass function
+  x_axis = c(0, 40)
+  y_axis = c(0, .25)
+  points = seq(from = x_axis[1], to = x_axis[2], by = 1)
+  
+  # n, p, c
+  params = matrix(c(40, .5, "red", 20, .3, "green", 30, .4, "yellow"), ncol = 3, byrow = TRUE)
+  dimnames(params) = list(NULL, c("n", "p", "c"))
+  
+  plot.new()
+  plot.window(xlim = x_axis, ylim = y_axis)
+  axis(1)
+  axis(2)
+  title(xlab = "points", ylab = "mass")
+  
+  
+  apply(params, 1, function(r) {
+    d = dbinom(points, as.numeric(r["n"]), as.numeric(r["p"]))
+    print(r["c"])
+    points(points, d, col = r["c"])
+  })
+}
+
+main <- function() {
+  plotBin()
+}
+
+main()
